779293757f2119d51eaa6d191a9a4963c8303b42
Igor <igorvinicius.only@gmail.com>
2021-10-21 21:13:04 -0300

    refazendo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,19 +1,18 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import logotipo from '../../assets/logo.svg'
+import { Container, Container2 } from './styles'
+
 
 export function Header() {
     return(
         <Container>
-            
-            <Content>
 
-                <img src={logoImg} alt="dt money" />
-                <button type='button'>
-                Nova transação
-                </button>
-            
-            </Content>
+            <Container2>
 
+            <img src={logotipo} alt="dt money" />
+            <button type='button' >Nova Transação</button>
+
+            </Container2>
+        
         </Container>
     )
 }
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+
     background: var(--brown);
 
 `;
 
-export const Content = styled.div`
+export const Container2 = styled.div`
+
     max-width: 1120px;
     margin: 0 auto;
-
     padding: 2rem 1rem 10rem;
     display: flex;
     align-items: center;
@@ -24,7 +25,6 @@ export const Content = styled.div`
         height: 3rem;
         
         transition: filter 0.2s;
-
         &:hover{
             filter:brightness(0.9) ;
         }
